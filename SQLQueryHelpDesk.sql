cf7123dca67fac42f5826d63d75d8c6ea3ebe903
Vinterdanilo <36777793+Vinterdanilo@users.noreply.github.com>
2018-10-20 04:00:57 -0300

    Informações DB

--- SQLQueryHelpDesk.sql
+++ SQLQueryHelpDesk.sql
@@ -4,31 +4,45 @@ go
 use HelpDesk
 go
 
-Create table Funcionarios(
-id_Funcionario int identity (1,1) primary key,
-rg_Funcionario varchar (10) not null,
-cpf_Funcionario varchar (11) not null,
-nome_Funcionario varchar (100) not null,
-tel1_Funcionario varchar (11) not null,
-tel2_Funcionario varchar (11) not null,
-email_Funcionario varchar (100) not null
+Create table TipoUsuario(
+id_TipoUsuario int identity (1,1) primary key,
+desc_TipoUsuario varchar (20) not null,
 )
 go
 
+insert into TipoUsuario values ('comum')   
+insert into TipoUsuario values ('administrador')   
+
 Create table Usuarios(
 id_Usuario int identity (1,1) primary key,
+id_TipoUsuario int not null,
 rg_Usuario varchar (10) not null,
 cpf_Usuario varchar (11) not null,
 nome_Usuario varchar (100) not null,
 tel1_Usuario varchar (11) not null,
 tel2_Usuario varchar (11) not null,
 email_Usuario varchar (100) not null
+CONSTRAINT  fk_TipoUsuario_Usuario  Foreign key (id_TipoUsuario) References TipoUsuario (id_TipoUsuario)
 )
 go
-drop table Chamados
+
+insert into Usuarios values (1,'377799725','44815102856','Danilo Vinter','985296363','969693636','danlo.vinter@hotmail.com')
+insert into Usuarios values (2,'456222233','25635563222','Adiministrador','982223336','970700777','administrador@live.com')
+
+Create table Acesso(
+id_Acesso int identity (1,1) primary key,
+id_Usuario int null,
+nome_Acesso varchar (15) not null,
+senha_Acesso varchar (8) not null,
+CONSTRAINT  fk_Usuario_Acesso  Foreign key (id_Usuario) References Usuarios (id_Usuario)
+)
+go
+
+insert into Acesso values ( 2,'user','12345678')   
+insert into Acesso values ( 3,'adm','senha123')       
+
 Create table Chamados(
 id_Chamado int identity (1,1) primary key,
-id_Funcionario int null,
 id_Usuario int null,
 Setor varchar (60) not null,
 Prioridade varchar (40) not null,
@@ -37,35 +51,14 @@ desc_Chamado varchar (255) not null ,
 dti_Chamado date not null,
 dtf_Chamado date null,
 solucao_Chamado varchar (255) null,
-CONSTRAINT  fk_Funionario_Chamado  Foreign key (id_Funcionario) References Funcionarios (id_Funcionario),
-CONSTRAINT  fk_Usuario_Chamado  Foreign key (id_Usuario) References Usuarios (id_Usuario),
+CONSTRAINT  fk_Usuario_Chamado  Foreign key (id_Usuario) References Usuarios (id_Usuario)
 );
 go
 
-Create table Acesso(
-id_Acesso int identity (1,1) primary key,
-id_Funcionario int null,
-id_Usuario int null,
-nome_Acesso varchar (15) not null,
-senha_Acesso varchar (8) not null,
-CONSTRAINT  fk_Funionario_Acesso  Foreign key (id_Funcionario) References Funcionarios (id_Funcionario),
-CONSTRAINT  fk_Usuario_Acesso  Foreign key (id_Usuario) References Usuarios (id_Usuario),
-)
-go
-
-select * from Funcionarios
-select * from Chamados
-insert into Chamados values 
-( null, 3, 'almoxarifado', 'Alta', null, 'estou com problemas para cadastrar chamadaos no sistema', '05/10/2018', null, null)
-
-select id_Funcionario from Acesso where nome_Acesso = 'user' and senha_Acesso = '12345678'
-select id_Usuario from Acesso where nome_Acesso = 'adm' and senha_Acesso = 'senha123' 
-
-insert into Acesso values ( NUll ,3,'user','12345678')   
-insert into Acesso values ( 1 ,NUll,'adm','senha123')    
-       
-select * from Acesso
 select * from Usuarios
 
-insert into Usuarios values ('0','0','Usuario Padrão','0','0','0')
-insert into Usuarios values ('377799725','44815102856','Danilo Vinter','985296363','969693636','danlo.vinter@hotmail.com')
+select * from TipoUsuario
+
+select * from Chamados
+
+select * from Acesso
